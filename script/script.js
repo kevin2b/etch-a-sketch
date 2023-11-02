554a669f4ad119ac9cf358cff7b7bd40030abf48
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-11-02 16:49:37 -0400

    Add reset button which allows users to clear grid and create another with specified size

--- script/script.js
+++ script/script.js
@@ -8,22 +8,46 @@ const leave = function leave (e){
 	return;
 }
 
+const resetButton = document.querySelector("#reset");
+const contentContainer = document.querySelector("#content");
+
 createGrid(16);
+
+resetButton.addEventListener("click", () => {
+	let sol = prompt("Enter the size (1-100) of the grid in terms of number of squares.");
+	if (sol === null || sol === ""){
+		return;
+	}
+		
+	if(+sol >= 1 && +sol <= 100){
+		resetGrid();
+		createGrid(sol);
+		return;
+	}
+	else{
+		alert("Invalid input. Enter a number between 1 and 100 inclusive.");
+	}
+	
+})
+
+function resetGrid(){
+	while (contentContainer.firstChild){
+		contentContainer.removeChild(contentContainer.firstChild);
+	}
+	return;
+}
 
 //Create size by size square grid for etch-a-sketch
 function createGrid(size){
-	const contentContainer = document.querySelector("#content");
-	
 	for (let row = 0; row < size; row++){	
-		//Size of grid in respect with 90% of width screen
-		const WIDTH = 70;
-		const percent = Math.floor(WIDTH/size);
+		const WIDTH = 50;
+		const percent = WIDTH/size;
 		const rowContainer = document.createElement("div");
-		rowContainer.setAttribute("style", `display: flex; justify-content: center; width:${WIDTH}%; flex-basis: ${percent}%;`);
+		rowContainer.setAttribute("style", `display: flex; justify-content: center; width:${WIDTH}%; flex: 0 0 ${percent}%;`);
 		for (let col = 0; col < size; col++){
 			const square = document.createElement("div");
 			square.setAttribute("style",
-			`padding-bottom: ${percent}%; flex-basis: ${percent}%; border: 1px solid black;`);
+			`padding-bottom: ${percent}%; flex: 0 0 ${percent}%; border: 1px solid black; `);
 			rowContainer.appendChild(square);
 			square.addEventListener("mouseenter", enter);
 			square.addEventListener("mouseleave", leave);
