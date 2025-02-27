f9edb95dccf5cde4d0a3b3d0a7ff041700a2a18f
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-02-27 09:33:31 -0500

    Add ability for user to set the size of the grid without changing grid dimensions

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-const container = document.querySelector(".container");
+initializeButton();
+createGrid(16);
 
-for (let i = 0; i < 16 * 16; i++){
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-
-  container.appendChild(cell);
-
-  cell.addEventListener("mouseover", (event)=>{
-    event.target.classList.add("enter");
+function initializeButton(){
+  const button = document.querySelector("button");
+  button.addEventListener("click", (event) => {
+    let size = prompt("Enter grid size between 2 and 100 inclusive:")
+    size = +size;
+    if (size >= 2 && size <= 100){
+      clearGrid();
+      createGrid(size);
+    }
   });
+}
 
-  cell.addEventListener("mouseleave", (event)=>{
-    event.target.classList.remove("enter");
-  });
+
+//Create a grid with hover effect on each cell
+function createGrid(size){
+  const container = document.querySelector(".container");
+  const CONTAINER_WIDTH = 960;
+  container.style.width = CONTAINER_WIDTH + "px";
+
+  for (let i = 0; i < size * size; i++){
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+
+    cell.style.width = CONTAINER_WIDTH/size + "px";
+    cell.style.height = CONTAINER_WIDTH/size + "px";
+  
+    cell.addEventListener("mouseover", (event)=>{
+      event.target.classList.add("enter");
+    });
+  
+    cell.addEventListener("mouseleave", (event)=>{
+      event.target.classList.remove("enter");
+    });
+  }
+}
+
+//Remove all cells
+function clearGrid(){
+  const cells = document.querySelectorAll(".cell");
+  for (const cell of cells){
+    cell.parentNode.removeChild(cell);
+  }
+
 }
 
