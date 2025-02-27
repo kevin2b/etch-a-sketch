b7d76c1f0b393bd113ed84d1fa11b714a0a8d63d
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-02-27 08:56:03 -0500

    Add hover effect over each element in the grid

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ for (let i = 0; i < 16 * 16; i++){
   cell.classList.add("cell");
 
   container.appendChild(cell);
+
+  cell.addEventListener("mouseover", (event)=>{
+    event.target.classList.add("enter");
+  });
+
+  cell.addEventListener("mouseleave", (event)=>{
+    event.target.classList.remove("enter");
+  });
 }
 
